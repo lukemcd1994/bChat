a7c36f34a331e03d382005bcd2d61f31a3c5df4c
Matt Davenport <mada6251@colorado.edu>
2018-10-31 10:07:03 -0600

    "Some styling changes, and added some components for Users and Chats"

---
+++ resources/js/components/ChatComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Chats</div>
+            <chat-messages-component></chat-messages-component>
+						<chat-form-component></chat-form-component>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
